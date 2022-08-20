3c0349ec1f14ece9facaee100894cf6709325dca
ALI AKBAR <captian.8089@gmail.com>
2022-08-20 13:36:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import Banner from './components/Banner/Banner'
+import NavBar from './components/NavBar/NavBar'
+import RowPost from './components/Row-Post/RowPost'
+import {action,originals} from "./urls"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title="Netflix Originals"/>
+      <RowPost url={action} title="Atcion" isSmall={true} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
